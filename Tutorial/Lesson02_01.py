45075dfa46de8869d46597f06a60097764593093
horkar667 <jmmpp413@yahoo.co.jp>
2022-02-02 13:53:50 +0900

    データ型

---
+++ Tutorial/Lesson02_01.py
@@ -0,0 +1,10 @@
+num01 = 123
+num02 = 1.23
+
+print(type(num01))
+print(type(num02))
+
+string_a = "Hello,World"
+
+print(string_a)
+print(type(string_a))
---
+++ Tutorial/Lesson02_02.py
@@ -0,0 +1,12 @@
+a = 10
+b = 1
+
+bool01 = (a > b)
+
+print(bool01)
+print(type(bool01))
+
+bool02 = (a < b)
+
+print(bool02)
+print(type(bool02))
