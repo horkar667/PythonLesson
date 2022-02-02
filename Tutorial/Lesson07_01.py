be78988db6fbf7342f1a10a69ee6ef0708775e65
horkar667 <jmmpp413@yahoo.co.jp>
2022-02-02 14:11:46 +0900

    関数

---
+++ Tutorial/Lesson07_01.py
@@ -0,0 +1,22 @@
+from unittest import result
+
+
+def say_hello():
+    print("hello world")
+
+
+say_hello()
+
+
+def say_hi(greeting):
+    print(greeting)
+
+
+say_hi("hi world")
+
+
+def add(num01, num02, num03):
+    return(num01 + num02 + num03) / 3
+
+
+print(add(9, 4, 2))
