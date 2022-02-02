120ec5201754981845b1617e6862eacbefff4360
horkar667 <jmmpp413@yahoo.co.jp>
2022-02-02 13:56:39 +0900

    演算子

---
+++ Tutorial/Lesson04_01.py
@@ -0,0 +1,27 @@
+x = 10
+y = 2
+
+print(x + y)
+print(x - y)
+print(x * y)
+print(x / y)
+print(x % y)
+
+print(x > y)
+print(x < y)
+
+print(x == y)
+print(x != y)
+
+print(x >= 5 and x <= 10)
+print(y >= 5 and y <= 10)
+
+print(x == 3 or y == 2)
+print(x == 3 or y == 3)
+
+z = 0
+x += 10
+z += y
+
+print(x)
+print(z)
