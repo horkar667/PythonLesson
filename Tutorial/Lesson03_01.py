26752e1d83742d5d9b19de58cf1e68342cf0723c
horkar667 <jmmpp413@yahoo.co.jp>
2022-02-02 13:55:36 +0900

    リスト

---
+++ Tutorial/Lesson03_01.py
@@ -0,0 +1,16 @@
+a = ["sato", "suzuki", "takahashi"]
+
+print(a[0])
+print(a[1])
+print(a[2])
+
+a[1] = "tanaka"
+
+print(a[1])
+
+b = [["kawasaki", "maeda"],["ikari", "koarai"]]
+
+print(b[0][0])
+print(b[0][1])
+print(b[1][0])
+print(b[1][1])
